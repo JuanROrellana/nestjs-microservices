16334cd42f1e2990a0cc4ee1aa8e8c70b039150e
Juan Jose Ramirez Orellana <ramirez.orellana.juanjose@gmail.com>
2022-09-09 18:02:40 -0600

    Add api-gateway complete

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { PassengerModule } from './passenger/passenger.module';
+import { FlightModule } from './flight/flight.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    }),
+    UserModule,
+    PassengerModule,
+    FlightModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
